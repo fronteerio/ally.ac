afcb2e27681bbafc83dd4082876f562df7b2a2b4
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2016-03-29 21:23:57 -0700

    First version of Ally website

---
+++ assets/js/ally.js
@@ -0,0 +1,36 @@
+$(document).on('ready', function() {
+
+    /**
+     * Show the form submit success message
+     */
+    var requestDemoSubmitted = function() {
+        $('.request-demo-component').toggleClass('hide');
+        $('#request-demo-success').focus();
+    };
+
+    /**
+     * Submit the contact form when valid
+     */
+    $('#request-demo-form').on('submit', function(e) {
+        if (!e.isDefaultPrevented()) {
+            var email = $.trim($('#request-demo-email').val());
+
+            $.ajax({
+                'url': 'http://formspree.io/info@ally.ac',
+                'method': 'POST',
+                'data': {
+                    'email': email,
+                    '_subject': 'Ally demo request'
+                },
+                'dataType': 'json',
+                'success': requestDemoSubmitted,
+                'error': function(err) {
+                    window.alert('An error has occurred. Please try again later.')
+                }
+            });
+
+            return false;
+        }
+    });
+
+});
