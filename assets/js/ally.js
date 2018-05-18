3513868fc5b0a640708176d449e027ca92804772
Sam Peck <sam@fronteer.io>
2018-05-18 12:40:27 +0100

    Initial design and dev

--- assets/js/ally.js
+++ assets/js/ally.js
@@ -248,3 +248,48 @@ $(document).on('ready', function() {
     }
 
 });
+
+    /**
+     * Smooth scrolling
+     */
+
+    $(document).ready(function(){
+      // Add smooth scrolling to all links
+      $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Store hash
+          var hash = this.hash;
+
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+            }, 800, function(){
+       
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+    });
+
+    /**
+     * Sticky heading
+     */
+
+    window.onscroll = function() {myFunction()};
+
+    var header = document.getElementById("myHeader");
+    var sticky = header.offsetTop;
+
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
+
+});
+
