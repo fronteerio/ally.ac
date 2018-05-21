6352efd6bcdb31d5005b3004bd05c40d314fc313
Simon Gaeremynck <gaeremyncks@gmail.com>
2018-05-21 08:45:36 +0200

    Lazy scroll the target element just below the navigational header

--- assets/js/ally.js
+++ assets/js/ally.js
@@ -258,20 +258,21 @@ $(document).on('ready', function() {
       $("a").on('click', function(event) {
 
         // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-          // Store hash
-          var hash = this.hash;
+        if (this.hash !== '') {
+            // Store hash
+            var hash = this.hash;
 
-          // Using jQuery's animate() method to add smooth page scroll
-          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top
-            }, 800, function(){
-       
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-          });
-        } // End if
+            // Using jQuery's animate() method to add smooth page scroll. Subtract the height of the navigational header
+            // so that the target element starts just below the navigation.
+            var headerNavHeight = $('nav ul').height();
+            var props = {
+                'scrollTop': $(hash).offset().top - headerNavHeight
+            };
+            $('html, body').animate(props, 800, function() {
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        }
     });
 
     /**
