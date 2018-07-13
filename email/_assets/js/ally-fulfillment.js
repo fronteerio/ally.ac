c996b8930e05baffd685c24c83965728d3bbaf29
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2018-07-13 11:31:21 +0100

    Moodle fulfilment

--- email/_assets/js/ally-fulfillment.js
+++ email/_assets/js/ally-fulfillment.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var values = $.url(document.location).param();
+    console.log(values);
     var template = _.template($("#template").html());
     $("#container").html(template(values));
 });
