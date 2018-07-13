a54b784b405c94706d0b3192972c63d71c1f0e1b
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2018-07-13 11:29:42 +0100

    Updated fulfilment email for self-hosted Moodle

--- email/_assets/js/ally-fulfillment.js
+++ email/_assets/js/ally-fulfillment.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     var values = $.url(document.location).param();
-    console.log(values);
     var template = _.template($("#template").html());
     $("#container").html(template(values));
 });
